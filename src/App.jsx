f2f1cdf968edea5b92f815244c5869b57f8e216d
Sunny <pandeysunny01@gmail.com>
2024-02-19 15:25:20 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+function App() {
+
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route>
+
+          </Route>
+        </Routes>
+      </Router>
+    </>
+  )
+}
+
+export default App
